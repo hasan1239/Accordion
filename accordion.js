0c66db4810029ab435c5c5c2e7ef2f23c12e5b05
Hasan Tariq <htariq0786@gmail.com>
2017-10-12 19:40:33 +0100

    1.1
    Added JSON code to dynamically populate the accordion

--- accordion.js
+++ accordion.js
@@ -12,3 +12,28 @@ for (i = 0; i < acc.length; i++) {
         }
     }
 }
+
+var div = document.getElementsByClassName('body')[0],
+	button = div.getElementsByTagName('button'),
+	p = div.getElementsByTagName('p'),
+	data = {
+		"blocks": [
+			{
+				"heading": "Section 1",
+				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore sed aliquam vitae fugit odit voluptate eligendi, blanditiis quasi nobis reiciendis! Rem maiores reprehenderit animi aliquam inventore vel adipisci delectus itaque! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore sed aliquam vitae fugit odit voluptate eligendi, blanditiis quasi nobis reiciendis!"
+			},
+			{
+				"heading": "Section 2",
+				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore sed aliquam vitae fugit odit voluptate eligendi, blanditiis quasi nobis reiciendis! Rem maiores reprehenderit animi aliquam inventore vel adipisci delectus itaque! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore sed aliquam vitae fugit odit voluptate eligendi, blanditiis quasi nobis reiciendis! Rem maiores reprehenderit animi aliquam inventore vel adipisci delectus itaque!"
+			},
+			{
+				"heading": "Section 3",
+				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+			}
+		]
+	};
+
+data.blocks.forEach(function(v, i) {
+	button[i].innerHTML = v.heading;
+	p[i].innerHTML = v.content;
+});
